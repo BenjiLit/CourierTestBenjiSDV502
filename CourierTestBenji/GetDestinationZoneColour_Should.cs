1c1d1740871052b8c394f9ef505a02b897ddd5ed
Benji Little <Benji-Little@live.nmit.ac.nz>
2017-08-15 11:28:12 +1200

    Add project files.

---
+++ CourierTestBenji/GetDestinationZoneColour_Should.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FastwayCourier;
+
+namespace CourierTestBenji
+{
+
+    [TestClass]
+    public class GetDestinationZoneColour_Should
+    {
+
+        [TestMethod]
+        public void ReturnZoneColour_Pink()
+        {
+            //Arrange
+            ParcelQuoteFromNelson lcQuoteFromNelson = new ParcelQuoteFromNelson();
+
+            //Act
+            string lcTicketColour = lcQuoteFromNelson.GetDestinationZone("Nelson"); //town name
+
+            //Assert
+            Assert.AreEqual("Pink", lcTicketColour); //ticket colour
+        }
+
+        [TestMethod]
+        public void ReturnZoneColour_Blue()
+        {
+            //Arrange
+            ParcelQuoteFromNelson lcQuoteFromNelson = new ParcelQuoteFromNelson();
+
+            //Act
+            string lcTicketColour = lcQuoteFromNelson.GetDestinationZone("Ward"); //town name
+
+            //Assert
+            Assert.AreEqual("Blue", lcTicketColour); //ticket colour
+        }
+
+        [TestMethod]
+        public void ReturnZoneColour_Lime()
+        {
+            //Arrange
+            ParcelQuoteFromNelson lcQuoteFromNelson = new ParcelQuoteFromNelson();
+
+            //Act
+            string lcTicketColour = lcQuoteFromNelson.GetDestinationZone("Murchison"); //town name
+
+            //Assert
+            Assert.AreEqual("Lime", lcTicketColour); //ticket colour
+        }
+
+        [TestMethod]
+        public void ReturnZoneColour_Orange()
+        {
+            //Arrange
+            ParcelQuoteFromNelson lcQuoteFromNelson = new ParcelQuoteFromNelson();
+
+            //Act
+            string lcTicketColour = lcQuoteFromNelson.GetDestinationZone("Reefton"); //town name
+
+            //Assert
+            Assert.AreEqual("Orange", lcTicketColour); //ticket colour
+        }
+
+             
+
+        //-----------------------------
+
+
+
+        [TestMethod]
+
+        public void OtherTest()
+        {
+            ParcelQuoteFromNelson lcQuoteFromNelson = new ParcelQuoteFromNelson();
+            string[] lcPinkTowns = new string[] { "Nelson", "Picton", "Hope" };
+
+            foreach (string lcTowns in lcPinkTowns)
+            {
+                string lcTicketColour = lcQuoteFromNelson.GetDestinationZone(lcTowns); //town name
+                Assert.AreEqual("Pink", lcTicketColour); //Ticket colour                             
+            }            
+        }
+                
+
+    }
+}
---
+++ CourierTestBenji/GetTicketPriceFromNelson_Should.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FastwayCourier;
+
+namespace CourierTestBenji
+{
+
+    [TestClass]
+    public class GetTicketPriceFromNelson_Should
+    {
+
+        [TestMethod]
+        public void ReturnTicketPrice_ToPinkZone()
+        {
+            //Arrange
+            ParcelQuoteFromNelson lcQuoteFromNelson = new ParcelQuoteFromNelson();
+
+            //Act
+            ParcelQuoteResult lcQuoteResult = lcQuoteFromNelson.CalculateQuote(12, "Pink"); //weight, destination zone
+
+            //Assert
+            Assert.AreEqual(0, lcQuoteResult.ExcessTickets); //excess tickets
+            Assert.AreEqual(4.15m, lcQuoteResult.Price); //total price
+        }
+
+        [TestMethod]
+        public void ReturnTicketPrice_ToBlueZone()
+        {
+            //Arrange
+            ParcelQuoteFromNelson lcQuoteFromNelson = new ParcelQuoteFromNelson();
+
+            //Act
+            ParcelQuoteResult lcQuoteResult = lcQuoteFromNelson.CalculateQuote(12, "Blue"); //weight, destination zone
+
+            //Assert
+            Assert.AreEqual(0, lcQuoteResult.ExcessTickets); //excess tickets
+            Assert.AreEqual(6.95m, lcQuoteResult.Price); //total price
+        }
+
+        [TestMethod]
+        public void ReturnTicketPrice_ToLimeZone()
+        {
+            //Arrange
+            ParcelQuoteFromNelson lcQuoteFromNelson = new ParcelQuoteFromNelson();
+
+            //Act
+            ParcelQuoteResult lcQuoteResult = lcQuoteFromNelson.CalculateQuote(12, "Lime"); //weight, destination zone
+
+            //Assert
+            Assert.AreEqual(0, lcQuoteResult.ExcessTickets); //excess tickets
+            Assert.AreEqual(8.70m, lcQuoteResult.Price); //total price
+        }
+
+        [TestMethod]
+        public void ReturnTicketPrice_ToOrangeZone()
+        {
+            //Arrange
+            ParcelQuoteFromNelson lcQuoteFromNelson = new ParcelQuoteFromNelson();
+
+            //Act
+            ParcelQuoteResult lcQuoteResult = lcQuoteFromNelson.CalculateQuote(12, "Orange"); //weight, destination zone
+
+            //Assert
+            Assert.AreEqual(0, lcQuoteResult.ExcessTickets); //excess tickets
+            Assert.AreEqual(12.95m, lcQuoteResult.Price); //total price
+        }
+
+
+
+
+    }
+}
